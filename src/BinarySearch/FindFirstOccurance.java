91d188dfa15aa12b786d8d4132303c774c1edd28
Tapan1111 <elevenshelby003@gmail.com>
2024-04-09 00:28:10 +0530

    Searching bs...

---
+++ src/BinarySearch/FindFirstOccurance.java
@@ -0,0 +1,50 @@
+package BinarySearch;
+
+public class FindFirstOccurance {
+	public static void main(String[] args) {
+		int[] arr = { 1, 1, 2, 2, 3, 3, 4, 5, 5, 5, 5 };
+		System.out.println(searchFirst(arr, 5));
+
+	}
+
+	public static int searchFirst(int[] arr, int target) {
+		int s = 0;
+		int e = arr.length - 1;
+		int ans = -1;
+		while (s <= e) {
+			int mid = s + (e - s) / 2;
+			if (arr[mid] == target) {
+				ans = mid;
+				e = mid - 1;
+			} else if (arr[mid] > target) {
+				e = mid - 1;
+
+			} else {
+				s = mid + 1;
+
+			}
+		}
+		return ans;
+	}
+
+	public static int searchLast(int[] arr, int target) {
+		int s = 0;
+		int e = arr.length - 1;
+		int ans = -1;
+		while (s <= e) {
+			int mid = s + (e - s) / 2;
+			if (arr[mid] == target) {
+				ans = mid;
+				e = mid - 1;
+			} else if (arr[mid] > target) {
+				e = mid - 1;
+
+			} else {
+				s = mid + 1;
+
+			}
+		}
+		return ans;
+	}
+
+}
