ebae68546c588079366fa5d9c28e934a5d821d48
Tapan1111 <elevenshelby003@gmail.com>
2024-05-05 23:10:10 +0530

    Implemented Doubly LinkedList and Circular Linked List...

--- src/LinkedList/LL.java
+++ src/LinkedList/LL.java
@@ -1,6 +1,7 @@
 package LinkedList;
 
 public class LL {
+	
 	private Node head;
 	private Node tail;
 	private int size;
@@ -8,15 +9,137 @@ public class LL {
 	public LL() {
 		this.size = 0;
 	}
-	
-	public void insertFirst(int val) {
-		Node node = new Node(val);
+
+	public Node find(int val) {
+		Node node = head;
+		while (node != null) {
+			if (node.value == val) {
+				return node;
+			}
+			node = node.next;
+		}
+		return null;
+	}
+	public int delete(int index) {
+		if (index == 0) {
+			return deleteFirst();
+		}
+		if (index == size) {
+			return deleteLast();
+		}
+		Node prev = get(index - 1);
+		int val = prev.next.value;
+		prev.next = prev.next.next;
+		size--;
+		return val;
+	}
+	public int deleteLast() {
+		if (size <= 1) {
+			return deleteFirst();
+		}
+
+		Node secondLast = get(size - 1);
+		int val = tail.value;
+		tail = secondLast;
+		tail.next = null;
+		size--;
+		return val;
+	}
+
+//	public int deleteLast() {
+//		if (size <= 1) {
+//			return deleteFirst();
+//		}
+//
+//		Node secondLast = get(size - 2);
+//		int val = tail.value;
+//		secondLast.next = null;
+//		tail = secondLast;
+//		size--;
+//		return val;
+//	}
+	public Node get(int index) {
+		Node temp = head;
+		for (int i = 1; i < index; i++) {
+			temp = temp.next;
+		}
+		return temp;
+
+	}
+
+	public int deleteFirst() {
+		int value = head.value;
+		head = head.next;
+		if (head == null) {
+			tail = null;
+		}
+		size--;
+		return value;
+	}
+
+	public void insertAtPosition(int value, int index) {
+		if (index == 0) {
+			insertFirst(value);
+			return;
+		}
+		if (index == size) {
+			insertLast(value);
+			return;
+		}
+		Node temp = head;
+		for (int i = 1; i < index; i++) {
+			temp = temp.next;
+		}
+		Node node = new Node(value, temp.next);
+		temp.next = node;
+		size++;
+		
+	}
+
+	public void insertFirst(int value) {
+		Node node = new Node(value);
 		node.next = head;
 		head = node;
 		if (tail == null) {
 			tail = head;
 		}
-		size += 1;
+		size++;
+	}
+
+	public void addLast(int value) {
+		if (head == null) {
+			head = new Node(value, null);
+			size = 1;
+			return;
+		}
+		Node temp = head;
+		while (temp.next != null) {
+			temp = temp.next;
+		}
+		Node node = new Node(value, null);
+		temp.next = node;
+		size++;
+	}
+
+	public void insertLast(int value) {
+		if (tail == null) {
+			insertFirst(value);
+			return;
+		}
+		Node node = new Node(value);
+		tail.next = node;
+		tail = node;
+		size++;
+	}
+
+	public void display() {
+		Node temp = head;
+		while (temp != null) {
+			System.out.print(temp.value + " ->");
+			temp = temp.next;
+		}
+		System.out.println("End");
+
 	}
 
 	private class Node {
@@ -31,6 +154,8 @@ public class LL {
 			this.value = value;
 			this.next = next;
 		}
+
 	}
+
 
 }
--- src/LinkedList/Main.java
+++ src/LinkedList/Main.java
@@ -3,8 +3,49 @@ package LinkedList;
 public class Main {
 	public static void main(String[] args) {
 
-		LL list = new LL();
-//		list.
+//		LL list = new LL();
+//		list.insertFirst(35);
+//		list.insertFirst(89);
+//		list.insertFirst(45);
+//		list.insertFirst(69);
+//		list.display();
+//		list.addLast(99);
+//		list.display();
+//		list.insertAtPosition(300, 3);
+//		list.display();
+//		System.out.println(list.find(300));
+//		list.display();
+
+//		DLL list = new DLL();
+//		list.insertFirst(3);
+//		list.insertFirst(2);
+//		list.insertFirst(8);
+//		list.insertFirst(17);
+//		list.display();
+//		list.insertLast(99);
+//		list.display();
+//		list.insert(8, 65);
+//
+//		list.display();
+
+		CLL list = new CLL();
+		list.insert(3);
+		list.insert(6);
+		list.insert(9);
+		list.insert(12);
+		list.insert(14);
+		list.display();
+		list.delete(12);
+		list.display();
+
+
+
+
+
+
+
+
+
 
 	}
 
