f92ddac224bc82eeef6e32bdebc0db5fead27df5
Tapan1111 <elevenshelby003@gmail.com>
2024-06-05 20:29:49 +0530

    practice LL

--- src/GfGArray/Start_01.java
+++ src/GfGArray/Start_01.java
@@ -64,13 +64,75 @@ public class Start_01 {
 		return res;
 	}
 
+	public static boolean checkIfSorted(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[i] > arr[j]) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	public static void reverse(int[] arr) {
+		int i = 0;
+		int j = arr.length - 1;
+		while (i < j) {
+			int temp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = temp;
+			i++;
+			j--;
+		}
+	}
+
+	public static int removeDuplicate(int[] arr) {
+		int res = 1;
+		for (int i = 1; i < arr.length; i++) {
+			if (arr[i] != arr[res - 1]) {
+				arr[res] = arr[i];
+				res++;
+			}
+		}
+
+		return res;
+	}
+
+	public static void moveZeros(int[] arr) {
+		int count = 0;
+		for (int i = 0; i < arr.length; i++) {
+			
+			if (arr[i] != 0) {
+				swap(arr[i], arr[i + 1]);
+				count++;
+			}
+		}
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i]);
+		}
+	}
+
+	private static void swap(int i, int j) {
+		int temp = i;
+		i = j;
+		j = temp;
+	}
+
 	public static void main(String[] args) {
-		int[] arr = { 3, 8, 12, 5, 6 };
+//		int[] arr = { 3, 8, 12, 15, 12, 15, 16 };
+		int[] arr = { 2, 4, 6, 0, 8, 0 };
 //		System.out.println(del(arr, 12));
 //		del(arr, 5);
 //		System.out.println(Arrays.toString(arr));
-		System.out.println(largestElement(arr));
-		System.out.println(secondLargetEfficient(arr));
+//		System.out.println(largestElement(arr));
+//		System.out.println(secondLargetEfficient(arr));
+//		System.out.println(checkIfSorted(arr));
+//		reverse(arr);
+//		System.out.println(Arrays.toString(arr));
+//		System.out.println(removeDuplicate(arr));
+		moveZeros(arr);
+//		System.out.println(Arrays.toString(arr));
 
 	}
 
--- src/LinkedList/GfGLinkedList.java
+++ src/LinkedList/GfGLinkedList.java
@@ -1,5 +1,8 @@
 package LinkedList;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+
 public class GfGLinkedList {
 	public static class Node {
 		int data;
@@ -148,6 +151,179 @@ public class GfGLinkedList {
 			return (res + 1);
 	}
 
+	public static Node sortedInsert(Node head, int x) {
+		Node t = new Node(x);
+		if (head == null) {
+			return t;
+		}
+		if (x < head.data) {
+			t.next = head;
+			return t;
+		}
+		Node curr = head;
+		while (curr.next != null && curr.next.data < x) {
+			curr = curr.next;
+
+		}
+		t.next = curr.next;
+		curr.next = t;
+		return head;
+	}
+
+	public static void printMiddle(Node head) {
+		if (head == null) {
+			return;
+		}
+		Node slow = head;
+		Node fast = head;
+		while (fast != null && fast.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+		}
+		System.out.println(slow.data);
+	}
+
+	public static void printMid(Node head) {
+		if (head == null) {
+			return;
+		}
+		int count = 0;
+		Node curr;
+		for (curr = head; curr != null; curr = curr.next) {
+			count++;
+		}
+		curr = head;
+		for (int i = 0; i < count / 2; i++) {
+			curr = curr.next;
+		}
+		System.out.println(curr.data);
+	}
+
+	public static void NthNodefromLast(Node head, int n) {
+		int count = 0;
+		Node t = head;
+		while (t != null) {
+			t = t.next;
+			count++;
+		}
+		System.out.println(count);
+		if (n > count) {
+			return;
+		}
+		Node curr = head;
+		for (int i = 1; i < (count - n + 1); i++) {
+			curr = curr.next;
+		}
+		System.out.println(curr.data);
+
+	}
+
+	public static void NthNodeEfficient(Node head, int n) {
+		Node c = head;
+		int count = 1;
+		while (count < n) {
+			c = c.next;
+			count++;
+		}
+		Node first = c;
+		Node second = head;
+		while (first != null && first.next != null) {
+			first = first.next;
+			second = second.next;
+		}
+		System.out.println(second.data);
+	}
+
+	public static Node revList(Node head) {
+		ArrayList<Integer> arr = new ArrayList<Integer>();
+		for (Node curr = head; curr != null; curr = curr.next) {
+			arr.add(curr.data);
+		}
+		for (Node curr = head; curr != null; curr = curr.next) {
+			curr.data = arr.remove(arr.size() - 1);
+		}
+		return head;
+	}
+
+	public static Node reverseLL(Node head) {
+		Node curr = head;
+		Node prev = null;
+		while (curr != null) {
+			Node next = curr.next;
+			curr.next = prev;
+			prev = curr;
+			curr = next;
+		}
+		return prev;
+	}
+
+	public static Node recRev(Node head) {
+		if (head == null || head.next == null) {
+			return head;
+		}
+		Node rest_head = recRev(head.next);
+		Node rest_tail = head.next;
+		rest_tail.next = head;
+		head.next = null;
+		return rest_head;
+	}
+
+	public static Node recRev2(Node curr, Node prev) {
+		if (curr == null) {
+			return prev;
+		}
+		Node next = curr.next;
+		curr.next = prev;
+		return recRev2(next, curr);
+	}
+
+	public static boolean detectLoop(Node head) {
+		Node temp = new Node(0);
+		Node curr = head;
+		while (curr != null) {
+			if (curr.next == null) {
+				return false;
+			}
+			if (curr.next == temp) {
+				return true;
+			}
+			Node n = curr.next;
+			curr.next = temp;
+			curr = curr.next;
+		}
+		return false;
+
+	}
+
+	public static boolean DetectLoopHash(Node head) {
+		HashSet<Node> set = new HashSet<>();
+		for (Node curr = head; curr != null; curr = curr.next) {
+			if (set.contains(curr)) {
+				return false;
+			}
+			set.add(curr);
+		}
+		return false;
+	}
+
+	public static void delNode(Node ptr) {
+		if (ptr == null || ptr.next == null) {
+			return;
+		}
+		ptr.data = ptr.next.data;
+		ptr.next = ptr.next.next;
+	}
+
+	public static Node swapped(Node head) {
+		Node curr = head;
+		while (curr.next != null) {
+			int temp = curr.data;
+			curr.data = curr.next.data;
+			curr.next.data = temp;
+			curr = curr.next.next;
+		}
+		return head;
+	}
 	public static void main(String[] args) {
 		Node head = null;
 		head = addAtLast(head, 10);
@@ -157,19 +333,41 @@ public class GfGLinkedList {
 		head = addAtLast(head, 50);
 		printLL(head);
 		System.out.println();
-		System.out.println(searchRecursive(head, 40));
+
+//		System.out.println();
+//		System.out.println(searchRecursive(head, 40));
 //		head = delHead(head);
 //		printLL(head);
 //		System.out.println();
 //		insertPos(head, 5, 45);
 //		printLL(head);
-		head = delHead(head);
-		System.out.println();
-		printLL(head);
-		delTail(head);
-		System.out.println();
+
+//		System.out.println();
+//		printLL(head);
+//		System.out.println();
+//		head = sortedInsert(head, 45);
+//		printLL(head);
+//		System.out.println();
+//		printMiddle(head);
+//		System.out.println();
+//		printMid(head);
+//		System.out.println();
+//		NthNodefromLast(head, 2);
+//		System.out.println();
+//		NthNodeEfficient(head, 2);
+//		Node curr = head;
+//		Node prev = null;
+//		head = recRev2(curr, prev);
+//		System.out.println(detectLoop(head));
+//		System.out.println(DetectLoopHash(head));
+////		head = recRev(head);
+//		Node ptr = new Node(30);
+		head = swapped(head);
+
 		printLL(head);
 
+
 	}
+
 
 }
